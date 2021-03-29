ce89c3dfd5c6c7b2eb0645eb194fd513b1eb333c
JulieLiu99 <xl2200@nyu.edu>
2021-03-29 15:10:50 +0400

    ex.1140

--- 1140. Stone Game II.py
+++ 1140. Stone Game II.py
@@ -12,20 +12,23 @@ class Solution:
         cache = [[0]*(n+1) for i in range(n)]
         
         def solve(i, M):
-            if i >= n: 
-                return 0
+            
+            if i >= n: return 0
+            
             M = min(M, n)
-            if cache[i][M] > 0:
-                return cache[i][M]
+            
+            if cache[i][M] > 0: return cache[i][M]
+            
             if (i+2*M) >= n:
                 cache[i][M] = sum(piles[i:n])
                 return cache[i][M]
+            
             best = -sys.maxsize - 1
             cur_sum = 0
             for l in range(1, 2*M+1):
                 cur_sum += piles[i+l-1]
                 best = max(best, cur_sum-solve(i+l, max(l,M)))
-                cache[i][M] = best
+            cache[i][M] = best
             return cache[i][M]
         
         return (sum(piles[0:n]) + solve(0,1)) // 2
