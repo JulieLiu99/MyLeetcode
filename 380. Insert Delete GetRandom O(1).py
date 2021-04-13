464bdf2fceae9782c639a537441cec6a1f6553dc
JulieLiu99 <xl2200@nyu.edu>
2021-04-13 11:58:59 +0400

    ex.380

---
+++ 380. Insert Delete GetRandom O(1).py
@@ -0,0 +1,62 @@
+class RandomizedSet:
+    
+    """
+    Hashmap (python dictionary) + List
+    
+    Keep track of the index of the added elements
+    To remove an element, copy the last one into its position and pop last.
+
+    list.append() takes O(1). 
+    dict.pop() and get/set take O(1).
+    
+    Time O(1)
+    Space O(n)
+    
+    """
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.nums, self.pos = [], {}
+        
+
+    def insert(self, val: int) -> bool:
+        """
+        Inserts a value to the set. Returns true if the set did not already contain the specified element.
+        """
+        if val not in self.pos:
+            self.nums.append(val)
+            self.pos[val] = len(self.nums) - 1
+            return True
+        return False
+
+    def remove(self, val: int) -> bool:
+        """
+        Removes a value from the set. Returns true if the set contained the specified element.
+        """
+        if val in self.pos:
+            idx = self.pos[val]
+            last = self.nums[-1]
+            
+            self.nums[idx] = last
+            self.pos[last] = idx
+            
+            self.nums.pop()
+            self.pos.pop(val, 0) #A value to return if the specified key do not exist. If this parameter is not specified, and the no item with the specified key is found, an error is raised
+            return True
+        
+        return False
+
+    def getRandom(self) -> int:
+        """
+        Get a random element from the set.
+        """
+        return self.nums[random.randint(0, len(self.nums) - 1)]
+
+
+# Your RandomizedSet object will be instantiated and called as such:
+# obj = RandomizedSet()
+# param_1 = obj.insert(val)
+# param_2 = obj.remove(val)
+# param_3 = obj.getRandom()
