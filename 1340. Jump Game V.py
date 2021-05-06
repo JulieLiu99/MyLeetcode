54ed39c4803282bfe286fcf050e536ca87ccd7ca
JulieLiu99 <xl2200@nyu.edu>
2021-05-06 12:36:55 +0400

    ex.1340

---
+++ 1340. Jump Game V.py
@@ -0,0 +1,36 @@
+class Solution:
+    def maxJumps(self, arr: List[int], d: int) -> int:
+        """
+        Sort and DP (Bottom Up)
+        
+        The number of jumps you can make from arr[i] only depends on the array members that are less than arr[i]. 
+        Sort the array first and deduce the results of the bigger elements from the smaller ones.
+        
+        Time O(nLogn)
+        Space O(n) to memoize jumps for every index.
+        
+        """
+        valueIndex = sorted([(x, i) for i, x in enumerate(arr)])
+        N = len(arr)
+        dp = [-1] * N
+        
+        for i in range(N):
+            
+            idx = valueIndex[i][1]
+            dp[idx] = 1
+            
+            for j in range(1, d+1):
+                if idx - j >= 0 and arr[idx - j] < arr[idx]:
+                    dp[idx] = max(dp[idx], dp[idx - j] + 1)
+                else:
+                    break
+                    
+            for j in range(1, d+1):
+                if idx + j < N and arr[idx + j] < arr[idx]:
+                    dp[idx] = max(dp[idx], dp[idx + j] + 1)
+                else:
+                    break
+        
+        return max(dp)
+
+
