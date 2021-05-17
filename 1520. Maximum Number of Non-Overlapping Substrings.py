c20e3361371b6be63a1c63c041ffe2cf63781a85
JulieLiu99 <xl2200@nyu.edu>
2021-05-17 18:55:25 +0400

    ex.1520

--- 1520. Maximum Number of Non-Overlapping Substrings.py
+++ 1520. Maximum Number of Non-Overlapping Substrings.py
@@ -5,15 +5,15 @@ class Solution:
         1. Get the first and last occurance of each characters
         2. For the range of each letter, update the range according to all the characters within the range. 
         E.g. "abab" has initial range a: [0,2], b: [1:3]. Since the range of a [0,2] includes b, we need to update the range of a to also include all occurances of b as well.
-        3. Sort all the ranges according to their ends. Take as many ranges as we can, as long as the start of the current range is >= the end of the previous range.
+        3. Sort all the ranges according to their ends. Take as many ranges as we can, as long as the start of the current range is > the end of the previous range.
 
         Time O(N)
         
         1) Loop through s once, so O(N) where N is the length of s
         2) Loop through all ranges, and there could be at most 26 ranges so O(26). Depending on how many other characters included in each range, we would be spending another 26 loops within the loop, so total O(26*26)
-        3) Loop through all the ranges generated from #2, so it's O(26*26). We also spent O(26*26log(26*26)) to sort it.
+        3) Loop through all the ranges generated from #2, so it's O(26). We also spent O(26log26) to sort it.
         
-        Space O(26) to store (start, end) for each char, and O(26*26) to store pairs:[l, r].
+        Space O(26) to store (start, end) for each char, and O(26) to store pairs:[l, r].
         
         """
           
@@ -36,7 +36,7 @@ class Solution:
         # Step 2: 
         # find all the correct boundries
         pairs = []
-        for char in set(s):
+        for char in start_end:
             l = tmpl = start_end[char][0]
             r = tmpr = start_end[char][1]
             while True:
@@ -54,9 +54,9 @@ class Solution:
         # greedily find the optimal solution
         pairs.sort(key=lambda x: x[1])
         res = []
-        prev_r = 0
+        prev_r = -1
         for l, r in pairs:
-            if l >= prev_r:
+            if l > prev_r:
                 res.append(s[l:r+1])
                 prev_r = r
         return res
