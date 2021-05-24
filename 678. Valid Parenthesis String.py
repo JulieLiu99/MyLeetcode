41754c1420f1a70146d7cc3c027b12a80b400f25
JulieLiu99 <xl2200@nyu.edu>
2021-05-24 15:11:48 +0400

    ex.678

---
+++ 678. Valid Parenthesis String.py
@@ -0,0 +1,23 @@
+class Solution:
+    def checkValidString(self, s: str) -> bool:
+        
+        # Go through s from both ends.
+        # (1)Treat * as left parenthesis (, make sure ( always more than or equal to ).
+        # (2)Treat * as right parenthesis ), make sure ) always more than or equal to (. 
+        # l_count(1) and r_count(2) check the balance of parenthese. 
+        # They can be larger than zero bc * can be empty, but not less than zero.
+        # If there are enough ('s and )'s', return True.
+        
+        n = len(s)
+        l_count = r_count = 0
+        
+        for i in range(n):
+            l_count += 1 if s[i] in '(*' else - 1
+            if l_count < 0: return False
+            
+        for i in range(n-1, -1, -1):
+            r_count += 1 if s[i] in ')*' else -1
+            if r_count < 0:
+                return False
+            
+        return True
