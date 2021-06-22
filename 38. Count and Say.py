0801fb9366a490bcd97a85ba8865a8729209f26b
JulieLiu99 <xl2200@nyu.edu>
2021-06-22 19:31:19 +0400

    ex.38

---
+++ 38. Count and Say.py
@@ -0,0 +1,49 @@
+class Solution:
+    def countAndSay(self, n: int) -> str:
+        
+        """
+        Recursive
+        
+        """
+#         if n == 1: return "1"
+        
+#         s = self.countAndSay(n-1)
+        
+#         i, char, tmp = 0, s[0], ''
+        
+#         for j in range(1, len(s)):
+#             # end of a contiguous section 
+#             if s[j] != char:
+#                 tmp += str(j-i) + char
+#                 i = j
+#                 char = s[j]
+#         # last contiguous section 
+#         tmp += str(len(s)-i) + char
+        
+#         return tmp
+    
+
+        """
+        Iterative
+        
+        """
+        # initilize result for countAndSay(1) = "1" 
+        result = '1'
+        
+        # already took care of first char
+        for _ in range(n-1):
+            prev = result
+            result = ''
+            j = 0
+            # start of a unique char
+            while j < len(prev):
+                char = prev[j]
+                count = 1
+                j += 1
+                # a contiguous section 
+                while j < len(prev) and prev[j] == char:
+                    count += 1
+                    j += 1
+                result += str(count) + str(char)
+                
+        return result
