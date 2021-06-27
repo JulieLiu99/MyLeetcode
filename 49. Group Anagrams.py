0ba2b11d3e5d296a1d333cfac2366b274803eb4a
JulieLiu99 <xl2200@nyu.edu>
2021-06-27 13:28:35 +0400

    ex.49

---
+++ 49. Group Anagrams.py
@@ -0,0 +1,20 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        """
+        Hashmap with sorted key
+        
+        Time O(nmlogm): Looping over strs O(n), Sorting each str O(mlogm) 
+        Space O(n)
+        
+        """
+        hashmap = {}
+        for st in strs:
+            # USE sorted() AND str.join() TO SORT A STRING ALPHABETICALLY:
+            # Sort string alphabetically and return list
+            # Combine list elements into one string
+            key = ''.join(sorted(st))
+            if key not in hashmap:
+                hashmap[key] = [st]
+            else:
+                hashmap[key] += [st]
+        return hashmap.values()
