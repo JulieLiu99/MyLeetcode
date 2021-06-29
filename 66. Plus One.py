12b8fe196acd636fdf78176a205ed86dedf39cf1
JulieLiu99 <xl2200@nyu.edu>
2021-06-29 21:04:59 +0400

    ex.66

--- 66. Plus One.py
+++ 66. Plus One.py
@@ -7,15 +7,16 @@ class Solution:
             digits[i] += 1
             
         else:
-            digits[i] = 0
-            # move to previous digit
-            i -= 1
-            # loop forward until digits[i] smaller than 9
+            # make digits[i] = 0 if originally 9
+            # move forward until digits[i] smaller than 9
             while i >= 0 and digits[i] == 9:
                 digits[i] = 0
                 i -= 1
+            # no digit smaller than 9 to increment
+            # add 1 at the begining, e.g. [9] -> [1, 0]
             if i < 0:
                 digits = [1] + digits
+            # add 1 to digits[i] that is smaller than 9 
             else:
                 digits[i] += 1
             
