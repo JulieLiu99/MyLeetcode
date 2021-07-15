0a456d12a295a3a000e670067b5168084fc7a43f
JulieLiu99 <xl2200@nyu.edu>
2021-07-15 09:50:00 +0400

    ex.491

---
+++ 491. Increasing Subsequences.py
@@ -0,0 +1,25 @@
+class Solution:
+    def findSubsequences(self, nums: List[int]) -> List[List[int]]:
+        
+        """
+        DFS
+        
+        Time O(2^n)
+        Space O(2^n)
+        
+        """
+        
+        self.res = set()
+        n = len(nums)
+        
+        def dfs(i, path):
+            if len(path) > 1:
+                # tuple because it's hashable, unlike list
+                self.res.add(tuple(path[:]))
+            
+            for j in range(i, n):
+                if i == 0 or path[-1] <= nums[j]:
+                    dfs(j+1, path+[nums[j]])
+        
+        dfs(0, [])
+        return list(self.res)
