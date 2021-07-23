2c7cb4fca3f72f1bc2fcba6e6bff028be5cc0703
JulieLiu99 <xl2200@nyu.edu>
2021-07-23 17:38:39 +0800

    ex.102

--- 102. Binary Tree Level Order Traversal.py
+++ 102. Binary Tree Level Order Traversal.py
@@ -24,16 +24,16 @@ class Solution:
 
             cur_level = []
             width = len(queue) # width of previous level
+            
             for i in range(width): 
                 node = queue.popleft()
+                cur_level.append(node.val)
                 if node.left:
                     queue.append(node.left)
                 if node.right:
                     queue.append(node.right)
-                cur_level.append(node.val)
                 
             res.append(cur_level)
             
         return res
-
 
