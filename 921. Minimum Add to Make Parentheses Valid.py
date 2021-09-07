0bcd2ceffebc59e70d6a6d12a885117bbb2d0171
JulieLiu99 <xl2200@nyu.edu>
2021-09-06 22:27:03 -0700

    ex.921

---
+++ 921. Minimum Add to Make Parentheses Valid.py
@@ -0,0 +1,28 @@
+class Solution:
+    def minAddToMakeValid(self, s: str) -> int:
+        
+        """
+        Minimum Add to Make Parentheses Valid
+        = Number of Invalid Parentheses
+        ^
+        Because we can't move things around, so whatever is extra, we add a complement to make it valid
+        
+        Time O(n)
+        Space O(1)
+        
+        """
+        
+        left = 0
+        res = 0
+        
+        for c in s:
+            if c == "(":
+                left += 1
+            else:
+                if left:
+                    left -= 1
+                else:
+                    res += 1
+                    
+        res += left
+        return res
