60451a4d94557e2ac332f139c354c133041ba2c5
JulieLiu99 <xl2200@nyu.edu>
2021-09-29 23:11:08 -0700

    ex.346

--- 133. Clone Graph.py
+++ 133. Clone Graph.py
@@ -16,16 +16,38 @@ class Solution:
         
         """
         
-        if not node: 
-            return node
+#         if not node: 
+#             return node
         
-        copy  = collections.defaultdict(list)
-        def dfs(node):
-            copy[node] = Node(node.val)     # copy node
-            for neighbor in node.neighbors: # copy node's neighbors
-                if neighbor not in copy: 
-                    dfs(neighbor)
-                copy[node].neighbors.append(copy[neighbor])
+#         copy  = collections.defaultdict(list)
+#         def dfs(node):
+#             copy[node] = Node(node.val)     # copy node
+#             for neighbor in node.neighbors: # copy node's neighbors
+#                 if neighbor not in copy: 
+#                     dfs(neighbor)
+#                 copy[node].neighbors.append(copy[neighbor])
                 
-        dfs(node)
+#         dfs(node)
+#         return copy[node]
+
+        """
+        BFS
+        
+        """
+        if not node: 
+            return
+        
+        copy = {node : Node(node.val)} # map original nodes to their clones
+        q = deque([node])
+
+        while q:
+            currNode = q.popleft()
+            for neighbor in currNode.neighbors:
+                if neighbor not in copy:
+                    copy[neighbor] = Node(neighbor.val) # store copy of the neighboring node
+                    q.append(neighbor)
+                # connect the node copy at hand to its neighboring nodes (also copies) -------- [1]
+                copy[currNode].neighbors.append(copy[neighbor])
+
+        # return copy of the starting node ------- [2]
         return copy[node]
---
+++ 346. Moving Average from Data Stream.py
@@ -0,0 +1,34 @@
+class MovingAverage:
+    """
+    Queue
+
+    next():
+    
+    Time: O(1): Queue takes O(1) to append and pop.
+
+    Space: O(n): n is the size of queue.The algorithm needs to maintain two integers: sum and numVal, and a queue. So it uses linear space.
+    
+    """
+
+    def __init__(self, size: int):
+        self.length = 0
+        self.max = size
+        self.sum = 0
+        self.queue = deque()
+
+    def next(self, val: int) -> float:
+        if self.length < self.max:
+            self.length +=1
+            self.queue.append(val)
+            self.sum += val
+        else:
+            self.sum -= self.queue.popleft()
+            self.queue.append(val)
+            self.sum += val
+        return self.sum/self.length
+
+
+
+# Your MovingAverage object will be instantiated and called as such:
+# obj = MovingAverage(size)
+# param_1 = obj.next(val)
