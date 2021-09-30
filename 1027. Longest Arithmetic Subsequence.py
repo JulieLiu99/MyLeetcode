853fe9775984158f79c13aab2f49b7ff4d6b2ac1
JulieLiu99 <xl2200@nyu.edu>
2021-09-30 09:10:17 -0700

    ex.1027

---
+++ 1027. Longest Arithmetic Subsequence.py
@@ -0,0 +1,28 @@
+class Solution:
+    def longestArithSeqLength(self, nums: List[int]) -> int:
+        
+        """
+        DP
+        
+        dp[index][diff]: length of arithmetic sequence at index with difference diff
+
+        Time O(N^2)
+        Space O(N^2)
+        
+        [3,6,9,12]:
+        
+        [{}, {3: 2}, {6: 2, 3: 3}, {9: 2, 6: 2, 3: 4}]
+        
+        """
+        
+        max_len = 0
+        dp = [{} for _ in range(len(nums))]  # index_of_array: {diff: curr_max_len}
+        for i in range(1, len(nums)):
+            for j in range(0, i):
+                diff = nums[i] - nums[j]
+                if diff in dp[j]:
+                    dp[i][diff] = dp[j][diff] + 1
+                else:
+                    dp[i][diff] = 2
+                max_len = max(max_len, dp[i][diff] )
+        return max_len
--- 695. Max Area of Island.py
+++ 695. Max Area of Island.py
@@ -7,49 +7,52 @@ class Solution:
         Space O(mn)
         
         """
-#         m, n = len(grid), len(grid[0])
-#         islands = Counter()
-#         color = 0
+        m, n = len(grid), len(grid[0])
+        islands = collections.defaultdict(int)
+        color = 0
         
-#         def dfs(color, i, j):
-#             if not 0<=i<m or not 0<=j<n or grid[i][j] != 1: 
-#                 return
-#             islands[color] += 1
-#             grid[i][j] = '#' # visited
-#             for x, y in [(1,0),(-1,0),(0,-1),(0,1)]: 
-#                 dfs(color, x+i, y+j)
+        def dfs(color, i, j):
+            if not 0<=i<m or not 0<=j<n or grid[i][j] != 1: 
+                return
+            islands[color] += 1
+            grid[i][j] = '#' # visited
+            for x, y in [(1,0),(-1,0),(0,-1),(0,1)]: 
+                dfs(color, x+i, y+j)
         
-#         for i in range(m):
-#             for j in range(n):
-#                 if grid[i][j] == 1:
-#                     dfs(color, i, j)
-#                     color += 1
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == 1:
+                    dfs(color, i, j)
+                    color += 1
 
-#         return max(list(islands.values()) + [0])
+        if not islands: 
+            return 0
+        else:
+            return max(islands.values())
 
         """
         BFS
 
         """
-        m, n = len(grid), len(grid[0])
+#         m, n = len(grid), len(grid[0])
         
-        def bfs(r, c):
-            q = deque([(r, c)])
-            grid[r][c] = 0
-            island_area = 0
-            while q:
-                r, c = q.popleft()
-                island_area += 1
-                for x, y in [(1,0),(-1,0),(0,-1),(0,1)]: 
-                    nr, nc = r + x, c + y
-                    if nr < 0 or nr == m or nc < 0 or nc == n or grid[nr][nc] == 0 or grid[nr][nc] == '#': continue
-                    grid[nr][nc] = '#' # visited
-                    q.append((nr, nc))
-            return island_area
+#         def bfs(r, c):
+#             q = deque([(r, c)])
+#             grid[r][c] = '#' # visited
+#             island_area = 0
+#             while q:
+#                 r, c = q.popleft()
+#                 island_area += 1
+#                 for x, y in [(1,0),(-1,0),(0,-1),(0,1)]: 
+#                     nr, nc = r + x, c + y
+#                     if nr >= 0 and nr < m and nc >= 0 and nc != n and grid[nr][nc] != 0 and grid[nr][nc] != '#': 
+#                         grid[nr][nc] = '#' # visited
+#                         q.append((nr, nc))
+#             return island_area
         
-        res = 0
-        for r in range(m):
-            for c in range(n):
-                if grid[r][c] == 1:
-                    res = max(res, bfs(r, c))
-        return res
+#         res = 0
+#         for r in range(m):
+#             for c in range(n):
+#                 if grid[r][c] == 1:
+#                     res = max(res, bfs(r, c))
+#         return res
