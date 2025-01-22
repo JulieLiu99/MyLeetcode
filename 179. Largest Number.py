f5c73cd936234a2c843840cc11c1c43cc183c71c
刘溪璇 <liuxixuan@macbookpro.mynetworksettings.com>
2025-01-21 21:18:52 -0500

    Update 179

--- 179. Largest Number.py
+++ 179. Largest Number.py
@@ -1,13 +1,34 @@
 class Solution:
 
+    # Custom comparator + default sort
+    # Time O(nlogn)
+    # Space O(n)
+
+    num_strings = [str(num) for num in nums]
+
+    def compare(n1, n2):
+        if n1 + n2 > n2 + n1:
+            return -1 # means n1 should come before n2
+        else:
+            return 1 # means n1 should come after n1
+
+    num_strings = sorted(num_strings, key=cmp_to_key(compare))
+
+    if num_strings[0] == "0":
+        return "0"
+
+    return "".join(num_strings)
+
 #     For each element, check if it needs to be swapped with any other.
 #     Compare function checks if the current order of elements gives max value.
     
 #     Time O(n^2)
 #     Space O(1)
-    
+
+    """
     def compare(self, n1, n2):
         return str(n1) + str(n2) >= str(n2) + str(n1)
+    """
     
 #     Bubble Sort
 #     Bubble Sort repeatedly swaps the adjacent elements if they are in wrong order.
@@ -27,7 +48,7 @@ class Solution:
 #     Selection Sort 
 #     Repeatedly finds the minimum element (considering ascending order) from unsorted part and putting it at the beginning. 
 
-    
+    """
     def largestNumber(self, nums: List[int]) -> str:
         for i in range(len(nums)-1, -1, -1):
             max_idx = 0
@@ -37,7 +58,7 @@ class Solution:
             nums[max_idx], nums[i] = nums[i], nums[max_idx]
         return str(int("".join(map(str, nums))))
     
-    
+    """
     # Insertion Sort
     # Values from the unsorted part are picked and placed at the correct position in the sorted part.
     
