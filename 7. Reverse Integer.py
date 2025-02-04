fb290f622e05ef4e11dfa043600242ed606a0422
Julie Liu <41465280+JulieLiu99@users.noreply.github.com>
2025-02-03 20:12:42 -0500

    Update 7. Reverse Integer.py

--- 7. Reverse Integer.py
+++ 7. Reverse Integer.py
@@ -2,51 +2,25 @@ class Solution:
     def reverse(self, x: int) -> int:
         
         """
-        :type x: int
-        :rtype: int
+        Use string as an intermediate representation
         
-        Time O(log(x)) 
+        Time O(digit) 
         floor(log10(x)) + 1 is the number of digits in integer x
         Space O(1) no extra data structure
         """
         
-        # res, sign = 0, 1
-        # if x < 0:
-        #     sign = -1
-        #     x = -1 * x
-        # while x != 0:
-        #     pop = x % 10            # pop last digit
-        #     x = int(x/10)           # retain the rest
-        #     res = res * 10 + pop    # append the popped digit to end
-        #     if (res > 2 ** 31 - 1) or (res > 2 ** 31 and sign == -1):
-        #         return 0
-        # return res * sign
+        max_int = pow(2, 31)-1
+        min_int = pow(-2, 31)
+
+        negative = True if x < 0 else False
         
-        negative = False
-        
-        if x < 0: 
-            negative = True
-            x = str(x)[1:] # get rid of '-'
-        else:
-            x = str(x)
-            
-        # get rid of ending 0's
-        end = len(x) - 1
-        while end >= 0 and x[end] == "0":
-            end -= 1
-        
-        # copy string from end to 0
-        res = ""
-        while end >= 0:
-            res += x[end]
-            end -= 1
-            
-        if not res: return 0 # res = '' for 0
-        
-        res = int(res)
-        if res > 2 ** 31 - 1 or (res == 2 ** 31 - 1 and negative):
+        str_x = str(abs(x))
+        reversed_str_x = str_x[::-1]
+
+        reversed_x = int(reversed_str_x)
+        if reversed_x > max_int or reversed_x < min_int:
             return 0
-        if negative: 
-            return -1 * res
+        elif negative:
+            return -1 * reversed_x
         else:
-            return res
+            return reversed_x
