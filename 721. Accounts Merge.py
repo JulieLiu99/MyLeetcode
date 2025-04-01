911174996ae3bae86addde5eb64045a4d7ff8342
刘溪璇 <liuxixuan@macbookpro.mynetworksettings.com>
2025-03-31 20:53:51 -0400

    721

--- 721. Accounts Merge.py
+++ 721. Accounts Merge.py
@@ -2,35 +2,39 @@ class Solution:
     def accountsMerge(self, accounts: List[List[str]]) -> List[List[str]]:
         """
         DFS 
+
+        Same as finding islands 
+        - In this case islands are accounts 
+        - Color is user (known through shared email)
         
-        Time O(n log n): for final sorting, other parts O(n)
-        Space O(n)
+        n is #accounts, k is #emails of account
+        Time O(nk log_nk): for final sorting, other parts O(nk)
+        Space O(nk)
         
         """
-        # 1. Build the graph of email -> all accounts accociated with the email owner
-        
-        email_to_account = defaultdict(list)
+        # 1. Build the graph of email -> accounts (of the same user)
+        email_to_accounts = defaultdict(list)
         for i, account in enumerate(accounts):
             for email in account[1:]:
-                email_to_account[email].append(i)
+                email_to_accounts[email].append(i)
                 
-        # 2. DFS for every account, find all emails accociated with the account user
-        merged_accounts = set()
+        # 2. DFS for every groups of accounts
+        seen_accounts = set()
         def dfs(i):
-            if i in merged_accounts:
+            if i in seen_accounts:
                 return set()
-            merged_accounts.add(i)
-            emails_of_accountUser = set()
+            seen_accounts.add(i)
+            all_emails_of_user = set()
             for email in accounts[i][1:]:
-                emails_of_accountUser.add(email)
-                for account in email_to_account[email]: # other accounts this email links to 
-                    emails_of_accountUser |= dfs(account)  # add their associated emails too
-                    # same as: emails_of_accountUser  = emails_of_accountUser.union(dfs(account))
-            return emails_of_accountUser
+                all_emails_of_user.add(email)
+                 # search through other accounts with the same email
+                for account in email_to_accounts[email]:
+                    all_emails_of_user |= dfs(account) 
+            return all_emails_of_user
         
         res = []
         for i, account in enumerate(accounts):
-            emails = dfs(i)  # find all emails belonging to the same account
+            emails = dfs(i)
             if emails:
                 res.append([account[0]] + sorted(list(emails)))
         return res
