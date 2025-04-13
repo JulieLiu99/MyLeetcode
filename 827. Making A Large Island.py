096bbeb4e4f7f6954a21b21b5dcded2bd2376abd
Julie Liu <41465280+JulieLiu99@users.noreply.github.com>
2025-04-13 07:50:45 -0400

    Update 827. Making A Large Island.py

--- 827. Making A Large Island.py
+++ 827. Making A Large Island.py
@@ -48,13 +48,13 @@ class Solution:
         for i in range(m):
             for j in range(n):
                 if grid[i][j] == 0:
-                    unique_land_id = set()
+                    neighbor_land_id = set()
                     for x, y in [(i+1,j), (i-1,j), (i,j+1), (i,j-1)]:
                         if 0 <= x < m and 0 <= y < n and grid[x][y] != 0:
-                            unique_land_id.add(grid[x][y])
+                            neighbor_land_id.add(grid[x][y])
                     # current 0 converted to 1
                     cur_area = 1
-                    for land_id in unique_land_id:
+                    for land_id in neighbor_land_id:
                         cur_area += area[land_id]
                     max_area = max(max_area, cur_area)
         return max_area
