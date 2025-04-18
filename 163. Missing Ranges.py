0cf4bf56d0ae04d75bf080e97578e781c982c616
Julie Liu <41465280+JulieLiu99@users.noreply.github.com>
2025-04-18 19:18:26 -0400

    Update 163. Missing Ranges.py

--- 163. Missing Ranges.py
+++ 163. Missing Ranges.py
@@ -1,41 +1,22 @@
 class Solution:
-    def findMissingRanges(self, nums: List[int], lower: int, upper: int) -> List[str]:
+    def findMissingRanges(self, nums: List[int], lower: int, upper: int) -> List[List[int]]:
         """
-        Traverse the list, compare adjacent elements
-        If not continuous, 
-            if differ by 2, res.append(str(num1+1))
-            else res.append(str(num1+1) + "->" + str(num2-1))
-        
+        Iterate through the list
+        Collect gaps between consecutive numbers
+
+        Add dummy edges lower-1 and upper+1 to handle edge cases
+
         Time O(n)
         Space O(1)
-        
         """
         res = []
+        prev = lower - 1
+        nums.append(upper + 1)
         
-        if not nums: 
-            if lower == upper:
-                return [str(lower)]
-            else:
-                return [str(lower) + "->" + str(upper)]
-        
-        if nums[0] > lower:
-            if lower + 1 == nums[0]:
-                res.append(str(lower))
-            else:
-                res.append(str(lower) + "->" + str(nums[0]-1))
-        
-        for i in range(1, len(nums)):
-            if nums[i-1] + 1 == nums[i]:
-                continue
-            elif nums[i-1] + 2 == nums[i]:
-                res.append(str(nums[i-1]+1))
-            else:
-                res.append(str(nums[i-1]+1) + "->" + str(nums[i]-1))
-                
-        if nums[-1] < upper:
-            if nums[-1] + 1 == upper:
-                res.append(str(upper))
-            else:
-                res.append(str(nums[-1]+1) + "->" + str(upper))
+        for num in nums:
+            if num - prev > 1: # gap
+                res.append([prev + 1, num - 1])
+            prev = num
         
         return res
+
