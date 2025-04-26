1da9f832bfdb80617e06d9f6ac5ff60a80c937fc
Julie Liu <41465280+JulieLiu99@users.noreply.github.com>
2025-04-26 14:15:35 -0400

    Create 2877. Create a DataFrame from List.py

---
+++ 2877. Create a DataFrame from List.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+def createDataframe(student_data: List[List[int]]) -> pd.DataFrame:
+    column_names = ["student_id", "age"]
+    result_dataframe = pd.DataFrame(student_data, columns=column_names)
+    return result_dataframe
