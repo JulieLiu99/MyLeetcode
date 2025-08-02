abac319edeb19ef4213060d6e53f23b30560d074
Julie Liu <41465280+JulieLiu99@users.noreply.github.com>
2025-08-02 15:35:59 +0200

    Update 317. Shortest Distance from All Buildings.py

--- 317. Shortest Distance from All Buildings.py
+++ 317. Shortest Distance from All Buildings.py
@@ -11,8 +11,8 @@ class Solution:
         
         BFS from buildings, keep counts of distances, record 0 locations accessable by every building on the board. Return the location with the min dist which can reach buildings.
         
-        Time O(n^2)
-        Space O(n)
+        Time O(B * R * C)
+        Space O(R * C)
         
         """
         rows = len(grid)
@@ -20,19 +20,21 @@ class Solution:
         
         if rows == cols == 1: return -1 # no place to build new building
         
-        dists = collections.defaultdict(int) # loc of empty spot: distance to all buildings it can reach
-        can_reach = collections.defaultdict(int)
+        dists = [[0] * cols for _ in range(rows)]
+        can_reach = [[0] * cols for _ in range(rows)]
+
         def bfs(row, col):
             q = collections.deque([(row, col, 0)])
             seen = set()
+            seen.add((row, col))
             while q:
                 r, c, d = q.popleft()
-                dists[(r, c)] += d
                 for y, x in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
                     nr = r + y
                     nc = c + x
                     if 0 <= nr < rows and 0 <= nc < cols and (nr, nc) not in seen and grid[nr][nc] == 0:
-                        can_reach[(nr, nc)] += 1 # space can reach this building
+                        can_reach[nr][nc] += 1 # space can reach this building
+                        dists[nr][nc] += d + 1
                         seen.add((nr, nc))
                         q.append((nr, nc, d+1)) # new loc has d + 1 distance from building
                         
@@ -44,54 +46,10 @@ class Solution:
                     buildings += 1               
 
         min_dist = float('inf')
-        for k in dists.keys():
-            if can_reach[k] == buildings:
-                min_dist = min(min_dist, dists[k])
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == 0 and can_reach[r][c] == buildings:
+                    min_dist = min(min_dist, dists[r][c])
         
         return min_dist if min_dist != float('inf') else -1
-
-        """
-        BFS from every empty spot
-        ^ Useful if we grid is mostly taken
-        
-        Correct but TLE
-        
-        """
-#         rows = len(grid)
-#         cols = len(grid[0])
-        
-#         if rows == cols == 1: return -1 # no place to build new building
-        
-#         dists = collections.defaultdict(int) # loc of empty spot: distance to all buildings it can reach
-#         can_reach = collections.defaultdict(int)
-#         def bfs(row, col):
-#             q = collections.deque([(row, col, 0)])
-#             seen = set()
-#             while q:
-#                 r, c, d = q.popleft()
-#                 for y, x in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
-#                     nr = r + y
-#                     nc = c + x
-#                     if 0 <= nr < rows and 0 <= nc < cols and (nr, nc) not in seen:
-#                         seen.add((nr, nc))
-#                         if grid[nr][nc] == 0:
-#                             q.append((nr, nc, d+1)) # new loc has d + 1 distance from starting point
-#                         elif grid[nr][nc] == 1:
-#                             dists[(row, col)] += d+1
-#                             can_reach[(row, col)] += 1 # space can reach this building
-                        
-#         buildings = 0
-#         for row in range(rows):
-#             for col in range(cols):
-#                 if grid[row][col] == 0: # from every space
-#                     bfs(row, col) 
-#                 elif grid[row][col] == 1:
-#                     buildings += 1               
-
-#         min_dist = float('inf')
-#         for k in dists.keys():
-#             if can_reach[k] == buildings:
-#                 min_dist = min(min_dist, dists[k])
-        
-#         return min_dist if min_dist != float('inf') else -1
 
