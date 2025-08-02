6dab69ce2572db5433fb477b4020561c117307c9
Julie Liu <41465280+JulieLiu99@users.noreply.github.com>
2025-08-02 16:08:42 +0200

    Update 317. Shortest Distance from All Buildings.py

--- 317. Shortest Distance from All Buildings.py
+++ 317. Shortest Distance from All Buildings.py
@@ -1,19 +1,17 @@
 class Solution:
     def shortestDistance(self, grid: List[List[int]]) -> int:
         """
-        BFS 
-        
-        0: empty land that you can pass by freely
-        1: building that you cannot pass through
-        2: obstacle that you cannot pass through
-        
-        -> build a house on an empty land that reaches all buildings in the shortest total travel distance
-        
-        BFS from buildings, keep counts of distances, record 0 locations accessable by every building on the board. Return the location with the min dist which can reach buildings.
-        
-        Time O(B * R * C)
-        Space O(R * C)
-        
+        BFS from buildings not lands
+        assumption is there are fewer buildings than lands
+
+        For each building:
+        - BFS traversal to get distance to all reachable lands
+        - +1 for number of buildings reachable by each land
+
+        Retun min distance for land reachable by all buildings
+
+        Time: O(B * R * C), B number of buildings, R rows, C cols
+        Space: O(R * C)
         """
         rows = len(grid)
         cols = len(grid[0])
@@ -27,21 +25,21 @@ class Solution:
             seen.add((row, col))
             while q:
                 r, c, d = q.popleft()
-                for y, x in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
-                    nr = r + y
-                    nc = c + x
+                for dr, dc in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
+                    nr = r + dr
+                    nc = c + dc
                     if 0 <= nr < rows and 0 <= nc < cols and (nr, nc) not in seen and grid[nr][nc] == 0:
-                        can_reach[nr][nc] += 1 # space can reach this building
+                        can_reach[nr][nc] += 1 # loc is reachable by building
                         dists[nr][nc] += d + 1
                         seen.add((nr, nc))
-                        q.append((nr, nc, d+1)) # new loc has d + 1 distance from building
+                        q.append((nr, nc, d+1)) # loc has d + 1 distance from building
                         
         buildings = 0
         for row in range(rows):
             for col in range(cols):
-                if grid[row][col] == 1: # from every building
-                    bfs(row, col)       
-                    buildings += 1               
+                if grid[row][col] == 1:
+                    buildings += 1
+                    bfs(row, col) # traverse from every building             
 
         min_dist = float('inf')
         for r in range(rows):
