fa99e7d13eb4c40b766c6ed89f1520ab7a7e8d7b
Julie Liu <41465280+JulieLiu99@users.noreply.github.com>
2025-08-24 21:11:05 -0400

    Create 1780. Check if Number is a Sum of Powers of Three.py

---
+++ 1780. Check if Number is a Sum of Powers of Three.py
@@ -0,0 +1,26 @@
+class Solution:
+    def checkPowersOfThree(self, n: int) -> bool:
+        """
+        Ternary representation
+        Same as binary representation but with base 3 instead of 2
+
+        Take n = 91
+
+        91 % 3 = 1 → ok, keep going. n = 30
+        30 % 3 = 0 → ok, keep going. n = 10
+        10 % 3 = 1 → ok, keep going. n = 3
+         3 % 3 = 0 → ok, keep going. n = 1
+         1 % 3 = 1 → ok, keep going. n = 0
+
+        91 = 81 + _ +  9  + _ + 1 
+           = 3⁴ + _ +  3² + _ + 3⁰
+
+        Time O(log_3 n)
+        Space O(1)
+        """
+        while n > 0:
+            if n % 3 == 2: # each power can only be present (1) or non present (2)
+                return False
+            # move to the next greater power
+            n //= 3
+        return True
