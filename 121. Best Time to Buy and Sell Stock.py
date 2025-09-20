45d965a6d0f9c8da3670e21983eec696f27c499e
Julie Liu <41465280+JulieLiu99@users.noreply.github.com>
2025-09-20 19:35:41 -0400

    Update 121. Best Time to Buy and Sell Stock.py

--- 121. Best Time to Buy and Sell Stock.py
+++ 121. Best Time to Buy and Sell Stock.py
@@ -1,38 +1,29 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        
         """
         Brute Force: for every day to buy, check every day to sell
         Time O(n^2)
-        
+
         Two Pointers
         One pass through the prices list
         
-        if prices[r] < prices[l]:
-            move l to r, and r to l+1
-        if prices[r] > prices[l]:
-            update res
-            move r one step right
+        Keep track of 
+        - Min price so far to buy
+        - Max possible profit from selling at each day
             
         Time O(n)
         Space O(1)
-        
         """
-        
-        res = 0
-        n = len(prices)
-        if n == 1: 
+        profit = 0
+        min_price = float('inf')
+
+        if not prices:
             return 0
-        
-        l = 0
-        r = 1
-        
-        while l<r<n:
-            if prices[r] < prices[l]:
-                l = r
-                r = l + 1
+
+        for price in prices:
+            if price < min_price:
+                min_price = price
             else:
-                res = max(res, prices[r] - prices[l])
-                r += 1
-        
-        return res
+                profit = max(profit, price - min_price)
+
+        return profit
