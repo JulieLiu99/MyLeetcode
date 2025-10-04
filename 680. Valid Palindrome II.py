91091457165f9626da0e42326ef492abb1f2ff3d
Julie Liu <41465280+JulieLiu99@users.noreply.github.com>
2025-10-04 10:59:52 +0800

    Update 680. Valid Palindrome II.py

--- 680. Valid Palindrome II.py
+++ 680. Valid Palindrome II.py
@@ -1,13 +1,16 @@
 class Solution:
     def validPalindrome(self, s: str) -> bool:
         """
-        Twp Pointers
+        Two Pointers
+        Move from two ends to center as long as characters are matching
+
+        Whenever characters are not matching
+        Delete char on either left pointer or right pointer
         
         Time O(n): two pointers go through string once, reverse O(substring)
         Space O(1): with reverse substring becomes O(n), could be reduced to O(1)
         
         """
-        delete = 0
         l = 0
         r = len(s) - 1
         
@@ -16,24 +19,6 @@ class Solution:
                 l += 1
                 r -= 1
             else:
+                # delete left or delete right
                 return s[l:r] == s[l:r][::-1] or s[l+1:r+1] == s[l+1:r+1][::-1]
-                """
-                delete += 1
-                if delete > 1:
-                    return False
-
-                # skip one of the two
-                # this doen't work because in this case
-                # lcupuu...uupucul
-                #  ^            ^
-                # both l += 1 and r -= 1 create matches
-                # but only one is correct
-                
-                if s[l+1] == s[r]:
-                    l += 1
-                elif s[l] == s[r-1]:
-                    r -= 1
-                else:
-                    return False
-                """
         return True
