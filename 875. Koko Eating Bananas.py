fb7975ce64f2f91a7d9a4a70107abb71f90e20dd
Julie Liu <41465280+JulieLiu99@users.noreply.github.com>
2025-10-19 10:13:16 -0400

    Update 875. Koko Eating Bananas.py

--- 875. Koko Eating Bananas.py
+++ 875. Koko Eating Bananas.py
@@ -2,21 +2,22 @@ class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
         """
         Binary search
-        Min of k is 1, bc k is integer
-        Max of k is max(piles), bc k > current pile, she only eats up the current pile
+        Range of target is [1, max size of a single pile]
         
         Time O(log(max(piles)) * N)
         Space O(1)
         
         """
-        l, r = 1, max(piles)
+        l = 1
+        r = max(piles)
         while l < r:
             m = l + (r-l) // 2
-            time = sum([math.ceil(i/m) for i in piles])
-            if time > h:
+            time = sum([math.ceil(pile/m) for pile in piles])
+            if time > h: # too slow
                 l = m + 1
             else:
                 r = m
         return l
+
 
 
