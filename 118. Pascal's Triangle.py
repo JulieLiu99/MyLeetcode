8e464943d1dd8d81edc3794227a6d548f41454fb
Julie Liu <41465280+JulieLiu99@users.noreply.github.com>
2025-10-26 15:47:27 -0400

    Update 118. Pascal's Triangle.py

--- 118. Pascal's Triangle.py
+++ 118. Pascal's Triangle.py
@@ -3,24 +3,25 @@ class Solution:
         
         """
         Apart from the first and final element 1,
-        Each row is derived by summing up pairs of elements from the previous row.
+        Each row is derived by summing up pairs of elements from the prev_rowious row.
         
         Time O(n): each element gets summed once or twice
         Space O(numRows): for tesmporary new_row[]
         
         """
         
-        res = [[1]]     # initialize first row
+        res = [[1]]  # first row
+
+        for _ in range(1, numRows):
+            prev_row = res[-1]
+            row = [1]
+            
+            # each inner element = sum of two elements above it
+            for i in range(1, len(prev_row)):
+                row.append(prev_row[i - 1] + prev_row[i])
+            
+            row.append(1)
+            res.append(row)
         
-        for n in range(1, numRows):   # size of previous row, from 1 to numRows-1
-            
-            new_row = [1]
-            i = 0   
-            while (i+1) < n:
-                new_row.append(res[-1][i] + res[-1][i+1])
-                i += 1
-            new_row += [1]
-            
-            res += [new_row]    # [new_row] instead of new_row because we want it to be subarray inside res[]
-            
         return res
+
