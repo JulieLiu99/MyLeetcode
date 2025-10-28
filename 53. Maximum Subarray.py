394eddecef9a7fbc5d7f28bcb97c4d69d6e3f3dd
Julie Liu <41465280+JulieLiu99@users.noreply.github.com>
2025-10-27 20:40:53 -0400

    Update 53. Maximum Subarray.py

--- 53. Maximum Subarray.py
+++ 53. Maximum Subarray.py
@@ -1,21 +1,33 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        
         """
-        DP
-        
-        At each index, keep track of the maximum sum using DP table
-        - either add the previous subarray
-        - or just the current num
-        Return the maximum out of the table
-        
+        DP table
+        to keep track of maximum sum at each index
+
         Time O(n)
         Space O(n)
-        
         """
-        dp = [0]*len(nums)
-        for i, num in enumerate(nums):            
-            dp[i] = max(dp[i-1] + num, num)
-        return max(dp)
+        # dp = [0]*len(nums)
+        # for i, num in enumerate(nums):            
+        #     dp[i] = max(dp[i-1] + num, num)
+        # return max(dp)
 
+        """
+        DP only keep track of max sum
+        
+        At each index, either
+        - extend the previous subarray
+        - or, start a new one with the current num
+
+        Time O(n)
+        Space O(1)
+        """
+        max_sum = nums[0]
+        current_sum = nums[0]
+
+        for num in nums[1:]:
+            current_sum = max(current_sum + num, num)
+            max_sum = max(max_sum, current_sum)
+
+        return max_sum
 
