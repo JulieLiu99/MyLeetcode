8513699e05c0de8e39fb0a3e3c083095868ae410
Julie Liu <41465280+JulieLiu99@users.noreply.github.com>
2025-12-27 14:38:16 -0500

    Update 3480. Maximize Subarrays After Removing One Conflicting Pair.py

--- 3480. Maximize Subarrays After Removing One Conflicting Pair.py
+++ 3480. Maximize Subarrays After Removing One Conflicting Pair.py
@@ -7,10 +7,23 @@ class Solution:
         - start_limit is the largest index `a` such that there exists a conflicting pair (a, b) with b <= r
         - Therefore, valid subarrays ending at `r` start in (start_limit + 1) .. r
 
+        Example:
+        conflicts = [(2, 4), (3, 5)]
+
+        At r = 4: (2, 4) forbids starts <= 2
+        => start_limit = 2
+        => valid subarrays ending at 4 start at 3 or 4
+
+        At r = 5: (3, 5) forbids starts <= 3
+        => start_limit = 3
+        => valid subarrays ending at 5 start at 4 or 5
+
         Time:  O(n + m)
         Space: O(n + m)
         """
 
+        # register conflicts by the ending value b, because
+        # the earliest time a conflict matters is when subarray’s right end reaches b
         conflicts_end_at = [[] for _ in range(n + 1)]
         for a, b in conflictingPairs:
             if a > b:
@@ -22,8 +35,8 @@ class Solution:
         prev_start_limit = 0 # second largest `a` seen so far
         gain = [0] * (n + 1)
 
+        # for each endpoint r, count valid subarrays (start_limit + 1) .. r
         for r in range(1, n + 1):
-            # for each endpoint r, count valid subarrays (start_limit + 1) .. r
             # each conflict (a, r) means subarrays ending at r cannot start <= a
             for a in conflicts_end_at[r]:
                 if a > start_limit:
